ecd3327d53ec66f63d2896d0fd835da6af477b64
enluce <86408122+enluce@users.noreply.github.com>
2025-03-19 19:10:55 +0530

    Initial Commit

--- Math Game App/Platforms/iOS/AppDelegate.cs
+++ Math Game App/Platforms/iOS/AppDelegate.cs
@@ -5,5 +5,5 @@ namespace Math_Game_App;
 [Register("AppDelegate")]
 public class AppDelegate : MauiUIApplicationDelegate
 {
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
